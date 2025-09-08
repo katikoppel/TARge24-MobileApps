4034607cde749c0217af6bb0916d47532b80b590
katikoppel <katikoppel1995@gmail.com>
2025-09-08 16:42:47 +0300

    Calcpage view

--- SciCalc/SciCalc/SciCalc/App.xaml.cs
+++ SciCalc/SciCalc/SciCalc/App.xaml.cs
@@ -1,15 +1,20 @@
-﻿namespace SciCalc
+﻿using SciCalc.Views;
+
+namespace SciCalc
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            MainPage = new CalculatorPage();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //{
+        //    return new Window(new AppShell());
+        //}
     }
 }
