40a930a2eed48fcfa1a47417883d34cf57ae0168
katikoppel <katikoppel1995@gmail.com>
2025-09-22 14:14:35 +0300

    Start page finished

--- MauiPlanets/MauiPlanets/Views/StartPage.xaml.cs
+++ MauiPlanets/MauiPlanets/Views/StartPage.xaml.cs
@@ -29,7 +29,12 @@ public partial class StartPage : ContentPage
 		parentAnimation.Add(0.6, 0.8, new Animation(v => imgUranus.Opacity = v, 0, 1, Easing.CubicIn));
 		parentAnimation.Add(0.7, 0.9, new Animation(v => imgNeptune.Opacity = v, 0, 1, Easing.CubicIn));
 
+		//intro box
+		parentAnimation.Add(0.7, 1, new Animation(v => imgIntro.Opacity = v, 0, 1, Easing.CubicIn));
 
 		parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
 	}
+
+	async void ExploreNow_Clicked(System.Object sender, System.EventArgs e)
+		=> Application.Current.MainPage = new NavigationPage(new PlanetsPage());
 }
